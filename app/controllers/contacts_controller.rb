4b2b1472578e0c4a00aea9a82cd65ced4db25494
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-02-09 18:07:55 -0500

    Rspec gem added. Contacts and Imports controllers refactored so current_user get only contacts and imports related to him.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -88,11 +88,11 @@ class ContactsController < SecureController
     @contacts = []
     
     imports.each do |import|
-      keys = [ params[:field1], params[:field2], params[:field3], params[:field4], params[:field5], "franchise", params[:field6] ]
-      values = [ import.name, import.birth_date, import.tel, import.address, import.credit_card, "", import.email]
+      keys = [ "user_id", params[:field1], params[:field2], params[:field3], params[:field4], params[:field5], "franchise", params[:field6] ]
+      values = [ current_user.id, import.name, import.birth_date, import.tel, import.address, import.credit_card, "", import.email]
       errors = ""
       # contact = Import.new(keys.zip(values).to_h) #Take as model Import, not contact yet
-      contact = Import.new(keys.zip(values).to_h)
+      contact = Contact.new(keys.zip(values).to_h)
 
       if contact.save
         refresh_file(import)
@@ -102,7 +102,7 @@ class ContactsController < SecureController
         end
         contact = nil
       else
-        import.update(import_errors: contact.errors.to_a)
+        import.update(import_errors: contact.errors.to_a.join(', '))
       end
 
       # if contact.name.match(/^[\p{L}\s\p{N}._@?¿!¡€-]+$/) == nil
@@ -133,28 +133,28 @@ class ContactsController < SecureController
       #   errors += "Email has errors. "
       # end
 
-      if errors == ""
-        new_contact = Contact.new(
-          user_id: current_user.id,
-          name: contact.name,
-          birth_date: Date.parse(contact.birth_date),
-          tel: contact.tel,
-          address: contact.address,
-          credit_card: contact.credit_card[-4..-1],
-          franchise: contact.franchise,
-          email: contact.email,
-          cc_digest: BCrypt::Password.create(contact.credit_card)
-          )
-        new_contact.save
-        refresh_file(import)
-        import.destroy
-        if Import.where(filename: import.filename) == []
-          Source.where(filename: import.filename).first.update(status: "finished")
-        end
-        contact = nil
-      else
-        import.update(import_errors: errors)
-      end
+      # if errors == ""
+      #   new_contact = Contact.new(
+      #     user_id: current_user.id,
+      #     name: contact.name,
+      #     birth_date: Date.parse(contact.birth_date),
+      #     tel: contact.tel,
+      #     address: contact.address,
+      #     credit_card: contact.credit_card[-4..-1],
+      #     franchise: contact.franchise,
+      #     email: contact.email,
+      #     cc_digest: BCrypt::Password.create(contact.credit_card)
+      #     )
+      #   new_contact.save
+      #   refresh_file(import)
+      #   import.destroy
+      #   if Import.where(filename: import.filename) == []
+      #     Source.where(filename: import.filename).first.update(status: "finished")
+      #   end
+      #   contact = nil
+      # else
+      #   import.update(import_errors: errors)
+      # end
       # @contacts << contact
     end
     redirect_to contacts_path
@@ -163,7 +163,7 @@ class ContactsController < SecureController
   private
 
     def get_contact
-      @contact = Contact.where(user_id: current_user.id, id: params[:id])
+      @contact = current_user.contacts.where(id: params[:id].to_i).first
     end
 
     def contact_params
--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,4 +1,5 @@
 class ImportsController < SecureController
+  before_action :get_import, only: [:show, :edit, :update, :destroy]
 
   def import
     Import.import(params[:file])
@@ -13,29 +14,28 @@ class ImportsController < SecureController
   end
 
   def show
-    @import = Import.find(params[:id])
   end
 
   def edit
-    @import = Import.find(params[:id])
   end
 
   def create
   end
 
   def update
-    import = Import.find(params[:id])
-    import.update(import_params)
+    @import.update(import_params)
     redirect_to import_path(import)
   end
 
   def destroy
-    import = Import.find(params[:id])
-    import.destroy
+    @import.destroy
     redirect_to imports_path
   end
 
   private
+    def get_import
+      @import = current_user.imports.where(id: params[:id].to_i).first
+    end
     def import_params
       params.require(:import).permit(:user_id, :import_errors, :name, :birth_date, :tel, :address, :credit_card, :franchise, :email)
     end
