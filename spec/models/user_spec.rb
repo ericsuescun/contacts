ef65684425223ef0d852d221d8ef0c30f0033b88
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-02-10 14:53:02 -0500

    User model tests completed with valid attributesa ans validations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "is valid with email and password" do
   	user = User.new(
-  		email: "edsa@hotmail.com",
+  		email: "someone@aservice.com",
   		password: "12345678"
   		)
   	expect(user).to be_valid
@@ -13,14 +13,25 @@ RSpec.describe User, type: :model do
   		email: "",
   		password: "12345678"
   		)
-  	expect(user).to be_invalid
+  	user.valid?
+  	expect(user.errors[:email]).to include("can't be blank")
   end
-  it "is invalid with password length under 6" do
+  it "is invalid with password length under 6 characters" do
   	user = User.new(
-  		email: "edsa@hotmail.com",
+  		email: "someone@aservice.com",
   		password: "12345"
   		)
   	expect(user).to be_invalid
   end
-  it "is invalid with a duplicate email" 
+  it "is invalid with a duplicate email" do
+  	user1 = User.create(
+  		email: "someone@aservice.com",
+  		password: "12345678"
+  		)
+  	user2 = User.create(
+  		email: "someone@aservice.com",
+  		password: "87654321"
+  		)
+  	expect(user2).to be_invalid
+  end
 end
