52078e918f244802039be967fc1d233677934d0d
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-02-10 11:35:19 -0500

    User model test implementation for validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with email and password" do
+  	user = User.new(
+  		email: "edsa@hotmail.com",
+  		password: "12345678"
+  		)
+  	expect(user).to be_valid
+  end
+  it "is invalid with empty email" do
+  	user = User.new(
+  		email: "",
+  		password: "12345678"
+  		)
+  	expect(user).to be_invalid
+  end
+  it "is invalid with password length under 6" do
+  	user = User.new(
+  		email: "edsa@hotmail.com",
+  		password: "12345"
+  		)
+  	expect(user).to be_invalid
+  end
+  it "is invalid with a duplicate email" 
 end
