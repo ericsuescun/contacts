5d977620a7e98fb8388c2a4c98fd3dcc0fc3aedd
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-02-10 17:31:48 -0500

    Contact model test implemented

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,9 +4,9 @@ class Contact < ApplicationRecord
   validates :name, format: { with: /^[\p{L}\s\p{N}._@?¿!¡€-]+$/, message: "character missmatch", multiline: true }
 
   # validates :birth_date, presence: true
-  validates :birth_date, format: { with: /\A\d{4}-\d{2}-\d{2}\Z/ || /\A\d{4}\d{2}\d{2}\Z/, message: "invalid format" }
+  validates :birth_date, format: { with: /\A\d{4}[\-*]\d{2}[\-*]\d{2}\Z/, message: "invalid format" }
 
-  validates :tel, format: { with: /\(\+\d{1,2}\)\s\d{3,3}\s\d{3,3}\s\d{2,2}\s\d{2,2}/, message: "phone number format is wrong" }
+  validates :tel, format: { with: /\(\+\d{1,2}\)\s\d{3,3}[\s-]\d{3,3}[\s-]\d{2,2}[\s-]\d{2,2}/, message: "phone number format is wrong" }
 
   validates :address, presence: true
 
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,4 +1,282 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
+
+	before do
+		@user = User.create(
+			email: "someone@aservice.com",
+			password: "12345678"
+			)
+	end
+
+	it "is valid with name, birth_date, tel, address, credit_card, franchise, email" do
+		contact = Contact.new(
+			user_id: @user.id,
+			name: "John-Smith",
+			birth_date: "1975-03-25",
+			tel: "(+57) 301 226 83 94",
+			address: "CR CL",
+			credit_card: "5300111122223333",
+			franchise: "",
+			email: "someoneelse@aservice.com"
+			)
+		expect(contact).to be_valid
+	end
+
+	it "is invalid with special characters except - in name" do
+		contact = Contact.new(
+			user_id: @user.id,
+			name: "John $!#%&/()[]{}?¿¡_;:+*^ Smith",
+			birth_date: "1975-03-25",
+			tel: "(+57) 301 226 83 94",
+			address: "CR CL",
+			credit_card: "5300111122223333",
+			franchise: "",
+			email: "someoneelse@aservice.com"
+			)
+		contact.valid?
+		expect(contact.errors[:name]).to include("character missmatch")
+	end
+
+	context "with birth date figures present" do
+		it "is valid without characters in between" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "19750325",
+				tel: "(+57) 301 226 83 94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			expect(contact).to be_valid
+		end
+
+		it "is valid with - characters in between" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 301 226 83 94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			expect(contact).to be_valid
+		end
+
+		it "is valid with / characters in between" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975/03/25",
+				tel: "(+57) 301 226 83 94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			expect(contact).to be_valid
+		end
+
+		it "is invalid with characters (including spaces) in between" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975 03:25",
+				tel: "(+57) 301 226 83 94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:birth_date]).to include("invalid format")
+		end
+
+		it "is invalid without separators and length less than 8" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975035",
+				tel: "(+57) 301 226 83 94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:birth_date]).to include("invalid format")
+		end
+
+		it "is invalid without separators and length more than 8" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "197503257",
+				tel: "(+57) 301 226 83 94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:birth_date]).to include("invalid format")
+		end
+	end
+
+	context "with tel phone number figures present" do
+		it "is valid with hyphens between last groups" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 301-226-83-94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			expect(contact).to be_valid
+		end
+
+		it "is valid with single digit country code" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+1) 301-226-83-94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			expect(contact).to be_valid
+		end
+
+		it "is invalid without characters between last groups" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 3012268394",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:tel]).to include("phone number format is wrong")
+		end
+
+		it "is invalid without country code" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "3012268394",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:tel]).to include("phone number format is wrong")
+		end
+
+		it "is invalid with wrong digits in area code" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 31-226-83-94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:tel]).to include("phone number format is wrong")
+		end
+
+		it "is invalid with wrong digits in first group after area code" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 301-26-83-94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:tel]).to include("phone number format is wrong")
+		end
+
+		it "is invalid with wrong digits in second group after area code" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 301-226-3-94",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:tel]).to include("phone number format is wrong")
+		end
+
+		it "is invalid with wrong digits in third group after area code" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 301-226-83-4",
+				address: "CR CL",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "someoneelse@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:tel]).to include("phone number format is wrong")
+		end
+	end
+
+	it "is invalid with address empty" do
+		contact = Contact.new(
+			user_id: @user.id,
+			name: "John Smith",
+			birth_date: "1975-03-25",
+			tel: "(+57) 301-226-83-94",
+			address: "",
+			credit_card: "5300111122223333",
+			franchise: "",
+			email: "someoneelse@aservice.com"
+			)
+		contact.valid?
+		expect(contact.errors[:address]).to include("can't be blank")
+	end
+
+	context "with email not empty" do
+		it "is invalid with no username" do
+			contact = Contact.new(
+				user_id: @user.id,
+				name: "John Smith",
+				birth_date: "1975-03-25",
+				tel: "(+57) 301-226-83-94",
+				address: "CL CR",
+				credit_card: "5300111122223333",
+				franchise: "",
+				email: "@aservice.com"
+				)
+			contact.valid?
+			expect(contact.errors[:email]).to include("has errors")
+		end
+	end
+	
 end
