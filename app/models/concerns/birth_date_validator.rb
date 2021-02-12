9e1179734371f013860632b9da8d2a147110c048
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-02-12 15:05:40 -0500

    Validations logic for birth_date and phone number refactored

--- app/models/concerns/birth_date_validator.rb
+++ app/models/concerns/birth_date_validator.rb
@@ -1,8 +1,6 @@
 class BirthDateValidator < ActiveModel::Validator
   def validate(contact)
-    if contact.birth_date.to_s.match(/\A\d{4}\-\d{2}\-\d{2}\z/) || contact.birth_date.to_s.match(/\A\d{4}\d{2}\d{2}\z/)
-      return true
-    else
+    unless contact.birth_date.to_s.match(/\A\d{4}\-\d{2}\-\d{2}\z/) || contact.birth_date.to_s.match(/\A\d{4}\d{2}\d{2}\z/)
       contact.errors.add :birth_date, "invalid format"
     end
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -19,9 +19,7 @@ class Contact < ApplicationRecord
   validate :phone_number_format
 
   def phone_number_format
-    if tel.match(/\(\+\d{1,2}\)\s\d{3,3}-\d{3,3}-\d{2,2}-\d{2,2}/) || tel.match(/\(\+\d{1,2}\)\s\d{3,3}\s\d{3,3}\s\d{2,2}\s\d{2,2}/)
-      return true
-    else
+    unless tel.match(/\(\+\d{1,2}\)\s\d{3,3}-\d{3,3}-\d{2,2}-\d{2,2}/) || tel.match(/\(\+\d{1,2}\)\s\d{3,3}\s\d{3,3}\s\d{2,2}\s\d{2,2}/)
       errors.add :tel, "phone number format is wrong"
     end
   end
