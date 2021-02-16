f1eef60a59ceeb842f369458f66bc59e7d5049b9
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-02-16 09:15:40 -0500

    get_franchise method transalted from contacts_controller to franchise_helper. it works on franchises.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -29,54 +29,7 @@ class ContactsController < SecureController
   end
 
   def get_franchise(contact)
-    franchises = Franchise.order("LENGTH(prefix) DESC")
-    franchises.each do |franchise|
-    # Franchise.all.each do |franchise|
-      #puts "Franchise: " + franchise.name
-      franchise.prefix.split(',').each do |prefix|
-        if prefix.match(/-/) != nil
-          (prefix.split('-')[0].to_i..prefix.split('-')[1].to_i).each do |range|
-            #puts "range: " + range.to_s
-            if contact.credit_card[0..(range.to_s.length - 1)] == range
-              franchise.number_length.split(',').each do |f_length|
-                if f_length.match(/-/) != nil
-                  (f_length.split('-')[0].to_i..f_length.split('-')[1].to_i).each do |l|
-                    if contact.credit_card.length == l
-                      contact.franchise = franchise.name
-                      return false
-                    end
-                  end
-                else
-                  if contact.credit_card.length == f_length.to_i
-                    contact.franchise = franchise.name
-                    return false
-                  end
-                end
-              end
-            end
-          end
-        else
-          if contact.credit_card[0..(prefix.length - 1)] == prefix
-            franchise.number_length.split(',').each do |f_length|
-              if f_length.match(/-/) != nil
-                (f_length.split('-')[0].to_i..f_length.split('-')[1].to_i).each do |l|
-                  if contact.credit_card.length == l
-                    contact.franchise = franchise.name
-                    return false
-                  end
-                end
-              else
-                if contact.credit_card.length == f_length.to_i
-                  contact.franchise = franchise.name
-                  return false
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-    return true
+    helpers.get_franchise(contact)
   end
 
   def refresh_file(import)
@@ -86,7 +39,7 @@ class ContactsController < SecureController
   def fix_header
     imports = Import.where(id: params[:imports_ids])
     @contacts = []
-    
+
     imports.each do |import|
       keys = [ "user_id", params[:field1], params[:field2], params[:field3], params[:field4], params[:field5], "franchise", params[:field6] ]
       values = [ current_user.id, import.name, import.birth_date, import.tel, import.address, import.credit_card, "", import.email]
--- app/helpers/franchises_helper.rb
+++ app/helpers/franchises_helper.rb
@@ -1,2 +1,53 @@
 module FranchisesHelper
+  def get_franchise(contact)
+    franchises = Franchise.order("LENGTH(prefix) DESC")
+    franchises.each do |franchise|
+    # Franchise.all.each do |franchise|
+      #puts "Franchise: " + franchise.name
+      franchise.prefix.split(',').each do |prefix|
+        if prefix.match(/-/) != nil
+          (prefix.split('-')[0].to_i..prefix.split('-')[1].to_i).each do |range|
+            #puts "range: " + range.to_s
+            if contact.credit_card[0..(range.to_s.length - 1)] == range
+              franchise.number_length.split(',').each do |f_length|
+                if f_length.match(/-/) != nil
+                  (f_length.split('-')[0].to_i..f_length.split('-')[1].to_i).each do |l|
+                    if contact.credit_card.length == l
+                      contact.franchise = franchise.name
+                      return false
+                    end
+                  end
+                else
+                  if contact.credit_card.length == f_length.to_i
+                    contact.franchise = franchise.name
+                    return false
+                  end
+                end
+              end
+
+            end
+          end
+        else
+          if contact.credit_card[0..(prefix.length - 1)] == prefix
+            franchise.number_length.split(',').each do |f_length|
+              if f_length.match(/-/) != nil
+                (f_length.split('-')[0].to_i..f_length.split('-')[1].to_i).each do |l|
+                  if contact.credit_card.length == l
+                    contact.franchise = franchise.name
+                    return false
+                  end
+                end
+              else
+                if contact.credit_card.length == f_length.to_i
+                  contact.franchise = franchise.name
+                  return false
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    return true
+  end
 end
