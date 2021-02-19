cd08f6b829785eb0fdb45282fdc827296f1f1b88
Eric Suescun <edsuescun@gmail.com>
2021-02-19 10:03:14 -0500

    Integration and request test deleted as test files for controller test not working properly. Controler test modified to describe without quotes so it can reach the rigth controller reference. Index specs now work ok.

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,41 +1,41 @@
 require 'rails_helper'
 require_relative '../support/devise'
 
-RSpec.describe "ContactsController", type: :controller do
-  before do
-    @user = create(:user)
-  end
+RSpec.describe ContactsController, type: :controller do
+  # before do
+  #   @user = create(:user)
+  # end
   describe "#index" do
     context "as a guest user" do
       it "gets a redirection" do
-        get contacts_path
+        get :index
         expect(response).to redirect_to("/users/sign_in")
       end
 
       it "returns a 302 http code for redirection" do
-        get contacts_path
+        get :index
         expect(response).to have_http_status "302"
       end
     end
 
     context "as an authenticated user" do
-      # login_user
+      login_user
 
       it "responds successfully" do
-        sign_in @user
-        get contacts_path
+        # sign_in @user
+        get :index
         expect(response).to have_http_status(:success)
       end
 
       it "returns a 200 http response code" do
-        sign_in @user
-        get contacts_path
+        # sign_in @user
+        get :index
         expect(response).to have_http_status "200"
       end
     end
 
     after do
-      @user.destroy
+      User.destroy_all
     end
 
   end
